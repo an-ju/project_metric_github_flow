fdf96e2c85fe0cee35cd0b3c953bdf93f4c12e48
An Ju <an_ju@berkeley.edu>
2017-04-27 16:59:45 -0700

    Version 0.1.0 done

--- lib/project_metric_github_flow.rb
+++ lib/project_metric_github_flow.rb
@@ -1,5 +1,45 @@
 require "project_metric_github_flow/version"
+require 'octokit'
+require 'json'
 
 class ProjectMetricGithubFlow
-  # Your code goes here...
+  attr_reader :raw_data
+
+  def initialize(credentials, raw_data = nil)
+    @project_url = credentials[:project]
+    @identifier = URI.parse(@project_url).path[1..-1]
+    @client = Octokit::Client.new access_token: credentials[:token]
+    @client.auto_paginate = true
+
+    @raw_data = raw_data
+  end
+
+  def image
+    refresh unless @raw_data
+    { chartType: 'commit_flow',
+      titleText: 'Commit Flow',
+      data: @raw_data }.to_json
+  end
+
+  def refresh
+    @raw_data = {commits: commits}
+  end
+
+  def raw_data=(new)
+    @raw_data = new
+    @score = nil
+    @image = nil
+  end
+
+  def score
+    refresh unless @raw_data
+    @score = @raw_data[:data].length
+  end
+
+  private
+
+  def commits
+    @client.commits @identifier
+  end
+
 end
